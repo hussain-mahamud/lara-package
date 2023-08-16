12915f8da6f16590e8f2ddfc4093ff1c47b2d471
Hussain Mahamud <hussainmahamud.swe@gmail.com>
2023-08-17 02:25:25 +0600

    Post Edit Added & Table Show Added

--- package/post/src/routes/web.php
+++ package/post/src/routes/web.php
@@ -2,5 +2,7 @@
 Route::group(['namespace'=>'Hussain\Post\Http\Controllers'],function(){
     Route::get('/post', 'PostController@index');
     Route::post('/post', 'PostController@store')->name('post.store');
+    Route::post('/edit-post/{id}', 'PostController@store')->name('post.edit');
+    Route::post('/delete-post/{id}', 'PostController@store')->name('post.delete');
 });
 
--- package/post/src/views/post.blade.php
+++ package/post/src/views/post.blade.php
@@ -17,8 +17,9 @@
         <div class="row">
             <div class="col-md-12">
                 <h1 class="text-center">Blog Post System</h1>
-                <form class="form-row" method="post">
+                <form class="form-row" method="post" id="post-form">
                     @csrf
+                    <div id="id-div"></div>
                     <div class="form-group">
                         <label for="title">Title</label>
                         <input type="text" name="title" class="form-control" required id="title">
@@ -33,6 +34,39 @@
                     </div>
                 </form>
             </div>
+        </div>
+        <div class="row" style="margin-top:30px;">
+            <div class="col-md-12">
+                <table class="table table-bordered">
+                    <thead>
+                        <tr>
+                            <th>SL</th>
+                            <th>Title</th>
+                            <th>Details</th>
+                            <th>Action</th>
+                        </tr>
+                    </thead>
+                    <tbody id="table-content">
+                        @php
+                            $count=1;
+                        @endphp
+                        @foreach($posts as $post)
+                            <tr>
+                                <td>{{$count++}}</td>
+                                <td>{{$post->title}}</td>
+                                <td>{{$post->content}}</td>
+                                <td>
+                                    <a data-id="{{$post->id}}"
+                                    type="button" class="btn btn-warning btn-sm" onclick="editPost(this)">Edit
+
+                                    </a>
+                                    <a id="{{$post->id}}" onclick="deletePost()" type="button" class="btn btn-danger btn-sm">Del</a></td>
+                            </tr>
+                        @endforeach
+                    </tbody>
+                </table>
+            </div>
+        </div>
     </div>
 </body>
 </html>
@@ -40,6 +74,7 @@
     function storePost(){
         let title=$('#title').val();
         let content=$('#content').val();
+        let id=$('#id').val();
         if(title=='' || content==''){
             Swal.fire({
                 // icon: 'warning',
@@ -54,27 +89,61 @@
             data:{
                 title:title,
                 content:content,
+                id:id,
                 _token:"{{csrf_token()}}"
             },
             success:function(data){
-                console.log(data)
-                if (data.success == false) {
+                console.log(data[0])
+                if (data[0].success==0) {
                     // Display SweetAlert2 error message for validation errors
                     Swal.fire({
                         icon: 'error',
                         title: 'Validation Error',
                         text: 'Please fix the following errors:',
-                        html: Object.values(data.errors).join('<br>')
+                        html: Object.values(data[0].errors).join('<br>')
                     });
                 } else {
-                    // Display SweetAlert2 success message
+                    let table_content='';
+                    let count=1;
+                    $.each(data[0].post, function(key, value){
+                        table_content+=`
+                            <tr>
+                                <td>${count++}</td>
+                                <td>${value.title}</td>
+                                <td>${value.content}</td>
+                                <td><a data-id="${value.id}" type="button" class="btn btn-warning btn-sm" onclick="editPost(this)">Edit</a>
+                                    <a id="${value.id}" onclick="deletePost()" type="button" class="btn btn-danger btn-sm">Del</a>
+                                </td>
+                            </tr>
+                        `;
+                    })
                     Swal.fire({
                         icon: 'success',
                         title: 'Success',
                         text: data.message
                     });
+                    $('#table-content').html(table_content);
+                    resetField();
                 }
             }
         })
+    }
+    function editPost(button){
+        let row = $(button).closest('tr');
+        $('tr').css('background-color', '');
+        row.css('background-color', '#e1f2e4');
+        let id = $(button).attr('data-id');
+        let title = row.find('td:eq(1)').text();
+        let content = row.find('td:eq(2)').text();
+        $('#title').val(title);
+        $('#content').val(content);
+        $('#post-form button[type="button"]').text('Update');
+        $('#id-div').html('<input type="hidden" name="id" value="'+id+'" id="id">');
+    }
+    function resetField(){
+        $('#id-div').html('');
+        $('#post-form button[type="button"]').text('Submit');
+        $('#title').val('');
+        $('#content').val('');
     }
 </script>
